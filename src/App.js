c2aa38b034be8b575a846c236505fbbc43e89557
unknown <ningukonin@gmail.com>
2025-03-03 14:42:32 +0530

    Frontend initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import FileUpload from "./components/FileUpload";
+import DownloadLink from "./components/DownloadLink";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [pdfUrl, setPdfUrl] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>DOCX to PDF Converter</h1>
+      <p>"Ningaraj Konin"</p>
+      <FileUpload setPdfUrl={setPdfUrl} />
+      <DownloadLink pdfUrl={pdfUrl} />
     </div>
   );
-}
+};
 
 export default App;
